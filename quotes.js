157a59b678c5ef85258bc7b45f105eb2a5583a2d
slv <silv3rr@users.noreply.github.com>
2018-01-03 20:44:12 +0100

    changed links to raw git

--- quotes.js
+++ quotes.js
@@ -1,13 +1,32 @@
 function showQuote() {
+  var quote = "";
+  var quoteDef = "&nbsp; &lt;pzs-ng&gt; Here's your quote sir: \" :)";
   try {
-    var quote = "";
-    var quoteDef = "&nbsp; &lt;pzs-ng&gt; Here's your quote sir: \" :)";
+    var quotesFile = new XMLHttpRequest();
+    quotesFile.onreadystatechange = function() {
+      if (quotesFile.readyState === 4 && quotesFile.status === 200) {
+        var allQuotes = quotesFile.responseText.split(/\r?\n/);
+        var i = Math.floor(Math.random() * (allQuotes.length-2));
+        if ( typeof allQuotes[i] === undefined || allQuotes[i] == null || allQuotes[i] === "" ) { allQuotes[i] = quoteDef; }
+        var quote = allQuotes[i].replace(/\</g,"&lt;").replace(/\>/g,"&gt;").replace(/(?:\\r\\n|\\r|\\n)/g,"<br> \n&nbsp ");
+        document.getElementById('randomQuote').innerHTML=( ' &nbsp ' + quote );
+      }
+    };
+    quotesFile.open("GET", "quotes", true);
+    quotesFile.send();
+  }
+  catch(e) { }
+}
+function showQuoteHTML() {
+  var quote = "";
+  var quoteDef = "&nbsp; &lt;pzs-ng&gt; Here's your quote sir: \" :)";
+  try {
     var quotesFile = new XMLHttpRequest();
     quotesFile.onreadystatechange = function() {
       if (quotesFile.readyState === 4 && quotesFile.status === 200) {
         var allQuotes = quotesFile.responseText.split(/(\r\n|\n)/g);
-        for ( i = Math.floor(Math.random() * (allQuotes.length-25)); i < allQuotes.length; i++ ) { 
-          if ( typeof allQuotes[i] === undefined || allQuotes[i] == null || allQuotes === "" ) { allQuotes[i] = quoteDef; }
+        for ( i = Math.floor(Math.random() * (allQuotes.length-25)); i < (allQuotes.length-25); i++ ) { 
+          if ( typeof allQuotes[i] === undefined || allQuotes[i] == null || allQuotes[i] === "" ) { allQuotes[i] = quoteDef; }
           if ( allQuotes[i].match(/^Quote #.*[0-9]/) ) {
             i++;
             while ( ! allQuotes[i].match(/^(Quote #.*[0-9]|Meet us at)/) ) {
@@ -21,7 +40,7 @@ function showQuote() {
           }
         }
       if ( typeof quote === undefined || quote == null || quote === "" ) { quote = quoteDef; }
-      document.getElementById('randomQuote').innerHTML=( quote );
+      document.getElementById('randomQuoteHTML').innerHTML=( quote );
       }
     };
     quotesFile.open("GET", "quotes.html", true);
