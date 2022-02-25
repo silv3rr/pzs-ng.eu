28fed9c5d74c8d4112170ad63c63e156ed4747a0
silver <silv3rr@users.noreply.github.com>
2022-02-25 21:27:39 +0100

    update scripts

--- scripts/template-mirror/tmm.pl
+++ scripts/template-mirror/tmm.pl
@@ -28,7 +28,6 @@ sub parse_template_line {
   shift;
   my $line = shift or die "Called parse_template_line with invalid parameters (missing third param (line))\n";
   $line =~ s/\%time_of_generation/scalar localtime/eg;
-
   if ($line =~ /\%quotes/) {
     my $quoteString = '';
     my $qnum = @quotes;
@@ -51,31 +50,29 @@ sub generate_page {
   my @lines = @_;
   open(PAGE, '>', "$vars{outputdir}/$page")
     or warn("Could not open '$vars{outputdir}/$page' for page generation. (OUTPUT FILE)") and return 0;
-
   foreach my $line (@lines) {
     if ($line eq '') { $line = ' ' }
     print PAGE parse_template_line($page, $line) . "\n";
   }
-
   close(PAGE);
-
   1;
 }
 
-$vars{'outputdir'} =~ s/\/+$//g; $vars{'templatedir'} =~ s/\/+$//g;
-
+$vars{'outputdir'} =~ s/\/+$//g;
+$vars{'templatedir'} =~ s/\/+$//g;
 print " * Start.\n";
-
 opendir(TEMPLATEDIR, $vars{'templatedir'});
 while ((my $entry = readdir(TEMPLATEDIR))) {
   my $fentry = "$vars{templatedir}/$entry";
   if (! -f $fentry) { next; }
   if ($entry =~ /^\./) { next; }
   if ($fentry !~ /\.tpl$/) { next; }
-
-   open(TEMPLATE, '<', "$vars{templatedir}/$entry");
+  open(TEMPLATE, '<', "$vars{templatedir}/$entry");
   $entry =~ s/\.tpl$//;
-  while (my $line = <TEMPLATE>) { $line =~ s/[\r\n]+$//g; push(@{$templates{$entry}}, $line); }
+  while (my $line = <TEMPLATE>) {
+    $line =~ s/[\r\n]+$//g;
+    push(@{$templates{$entry}}, $line);
+  }
   close(TEMPLATE);
 }
 closedir(TEMPLATEDIR);
@@ -83,21 +80,24 @@ closedir(TEMPLATEDIR);
 if ( $quotes_only == 1 ) {
   print "   - Generating quotes page only.\n";
   $page = "quotes.html";
+  print "    - Generating $page.\n";
+  generate_page($page, @{$templates{$page}});
   if ($0 =~ /pre-commit/ || $ENV{'CI'}) {
     print "    - Checking quotes for changes.\n";
-    @diff= qx/git diff --name-status quotes/;
-    foreach (@diff) {
-      if ($_ =~ /^M\s+quotes$/) {
-        print "    - Generating $page.\n";
-        generate_page($page, @{$templates{$page}});
-        print "    - Running git add.\n";
-        @out = qx/git add -v quotes.html/;
-        print(@out);
+    if (qx/git diff --name-status .\/quotes/ =~ /^M\s+quotes$/) {
+      print "    - Generating $page.\n";
+      generate_page($page, @{$templates{$page}});
+      print "    - Running git add.\n";
+      system("git", "add", "quotes.html");
+      if ($ENV{'CI'}) {
+        system("git", "config", "--global", "user.name", "$ENV{'GIT_USER_NAME'}");
+        system("git", "config", "--global", "user.email", "$ENV{'GIT_USER_EMAIL'}");
+        print "    - Running git commit.\n";
+        system("git", "commit", "-m", "quotes++", "quotes.html");
+        print "    - Running git push.\n";
+        system("git", "push");
       }
     }
-  } else {
-    print "    - Generating $page.\n";
-    generate_page($page, @{$templates{$page}});
   }
 } else {
   print "   - Generating pages.\n";
