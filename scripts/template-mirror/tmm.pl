d9e6ce21d6d53d8e385083f6f4bba8bcbf7b590b
silver <silv3rr@users.noreply.github.com>
2022-02-25 20:36:59 +0100

    add scripts

---
+++ scripts/template-mirror/tmm.pl
@@ -0,0 +1,112 @@
+#!/usr/bin/perl
+
+# tmm.pl - template-mirror-mini ;)
+# doesnt rly mirror anything, just creates quotes.html (and if you want, the other pages too)
+# original code by the legendary daxxar ofc
+
+my $quotes_only = 1;
+
+my %vars = (
+  #templatedir => 'templates',
+  #quotes_file => 'quotes',
+  #outputdir => 'pages'
+  #templatedir => $ENV{'HOME'} . '/dev/pzs-ng-scripts/template-mirror/templates',
+  #quotes_file => $ENV{'HOME'} . '/dev/pzs-ng.eu/quotes',
+  #outputdir => $ENV{'HOME'} . '/dev/pzs-ng.eu'
+  templatedir => './scripts/template-mirror/templates',
+  quotes_file => './quotes',
+  outputdir => '.'
+);
+my %templates;
+
+my @quotes;
+open(QUOTES, $vars{'quotes_file'});
+push(@quotes, $_) while <QUOTES>;
+close(QUOTES);
+
+sub parse_template_line {
+  shift;
+  my $line = shift or die "Called parse_template_line with invalid parameters (missing third param (line))\n";
+  $line =~ s/\%time_of_generation/scalar localtime/eg;
+
+  if ($line =~ /\%quotes/) {
+    my $quoteString = '';
+    my $qnum = @quotes;
+    for (reverse @quotes) {
+      my $quote = $_;
+      $quoteString .= 'Quote #<b>' . $qnum-- . "</b><br>\n";
+      $quoteString .= "-------------<br>\n";
+      $quote = ' &nbsp; ' . $quote;
+      $quote =~ s/</&lt;/g; $quote =~ s/>/&gt;/g;
+      $quote =~ s/\\n/<br>\n &nbsp; /g;
+      $quoteString .= $quote . "<br><br>\n";;
+    }
+    $line =~ s/\%quotes/$quoteString/g;
+  }
+  return $line;
+}
+
+sub generate_page {
+  my $page = shift or die "Called generate_page with invalid parameters (missing second param (page))\n";
+  my @lines = @_;
+  open(PAGE, '>', "$vars{outputdir}/$page")
+    or warn("Could not open '$vars{outputdir}/$page' for page generation. (OUTPUT FILE)") and return 0;
+
+  foreach my $line (@lines) {
+    if ($line eq '') { $line = ' ' }
+    print PAGE parse_template_line($page, $line) . "\n";
+  }
+
+  close(PAGE);
+
+  1;
+}
+
+$vars{'outputdir'} =~ s/\/+$//g; $vars{'templatedir'} =~ s/\/+$//g;
+
+print " * Start.\n";
+
+opendir(TEMPLATEDIR, $vars{'templatedir'});
+while ((my $entry = readdir(TEMPLATEDIR))) {
+  my $fentry = "$vars{templatedir}/$entry";
+  if (! -f $fentry) { next; }
+  if ($entry =~ /^\./) { next; }
+  if ($fentry !~ /\.tpl$/) { next; }
+
+   open(TEMPLATE, '<', "$vars{templatedir}/$entry");
+  $entry =~ s/\.tpl$//;
+  while (my $line = <TEMPLATE>) { $line =~ s/[\r\n]+$//g; push(@{$templates{$entry}}, $line); }
+  close(TEMPLATE);
+}
+closedir(TEMPLATEDIR);
+
+if ( $quotes_only == 1 ) {
+  print "   - Generating quotes page only.\n";
+  $page = "quotes.html";
+  if ($0 =~ /pre-commit/ || $ENV{'CI'}) {
+    print "    - Checking quotes for changes.\n";
+    @diff= qx/git diff --name-status quotes/;
+    foreach (@diff) {
+      if ($_ =~ /^M\s+quotes$/) {
+        print "    - Generating $page.\n";
+        generate_page($page, @{$templates{$page}});
+        print "    - Running git add.\n";
+        @out = qx/git add -v quotes.html/;
+        print(@out);
+      }
+    }
+  } else {
+    print "    - Generating $page.\n";
+    generate_page($page, @{$templates{$page}});
+  }
+} else {
+  print "   - Generating pages.\n";
+  foreach my $page (keys %templates) {
+    print "    - Generating $page.\n";
+    generate_page($page, @{$templates{$page}});
+  }
+}
+
+print " + Done.\n";
+
+0;
