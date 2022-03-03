c75dd621b5af3cf032bba996990a675c6e22ed2e
silver <silv3rr@users.noreply.github.com>
2022-03-03 22:14:48 +0100

    tmm.pl

--- scripts/template-mirror/tmm.pl
+++ scripts/template-mirror/tmm.pl
@@ -82,6 +82,7 @@ if ( $quotes_only == 1 ) {
   if ($0 =~ /pre-commit/ || $ENV{CI}) {
     print "  - Checking quotes for changes.\n";
     $diff = qx(git diff --name-only --diff-filter=ACMRT HEAD^ -- $vars{quotes_file});
+    chomp($diff);
     print("diff=$diff vars_qf=$vars{quotes_file}");
     if ($diff == $vars{quotes_file}) {
     print "    - No changes, exiting...\n";
