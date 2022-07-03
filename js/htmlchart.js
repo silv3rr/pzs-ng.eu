d20bfdddf713b510194be9672a948badade95ae4
silver <silv3rr@users.noreply.github.com>
2022-07-03 17:32:04 +0200

    mv func to js, fix labels, cleanup html

--- js/htmlchart.js
+++ js/htmlchart.js
@@ -1,12 +1,12 @@
 /*
  * Create actual chart from table data using ChartJS
  */
-function BuildChart(chartName, chartTitle, labelTitle, data, horizontal) {
+function buildChart(chartName, chartTitle, labelTitle, labels, data, horizontal) {
     Chart.defaults.set('plugins.datalabels', {
         color: '#FFF' // Default color
     });
     var ctx = document.getElementById(chartName).getContext('2d');
-    var chart = new Chart(ctx, {
+    return new Chart(ctx, {
         type: 'bar',
         data: {
             labels: labels,
@@ -37,6 +37,13 @@ function BuildChart(chartName, chartTitle, labelTitle, data, horizontal) {
             layout: {
                 padding: 10
             },
+            scales: {
+                x: {
+                    ticks: {
+                        maxTicksLimit: 99,
+                    }
+                }
+            },
             plugins: {
                 title: {
                     display: true,
@@ -57,7 +64,7 @@ function BuildChart(chartName, chartTitle, labelTitle, data, horizontal) {
             },
         }       
     });
-    return chart
+    //return chart
 }
 Chart.register(ChartDataLabels);
 
@@ -72,7 +79,7 @@ function tableJson(tableName) {
         headers[i] = table.rows[0].cells[i].innerHTML.toLowerCase().replace(/ /gi, '');
     }
     // Go through cells 
-    for (i = 1; i < table.rows.length; i++) {
+    for (let i = 1; i < table.rows.length; i++) {
         var tableRow = table.rows[i];
         var rowData = {};
         for (let j = 0; j < tableRow.cells.length; j++) {
@@ -81,4 +88,42 @@ function tableJson(tableName) {
         json.push(rowData);
     }
     return json
+}
+
+/*
+ * Parse table data from html
+ */
+ function parseTableData() {
+
+    var filter = []
+    var labels = []
+    var data = []
+
+    // dataTableAuthor
+
+    let chartName = 'authorChart'
+    let chartTitle = 'authors avg # of commits per year'
+    let labelTitle = 'commits per year'
+    let json = tableJson('dataTableAuthor')
+    filter = json.filter(e => e.commits > 10) // need at least 10 commits
+    filter.forEach(e => {
+        labels.push([Object.values(e)[0], `(${String(e.to-e.from)}y)`])
+    });
+    for (let i = 0; i < filter.length; i++) {
+        data[i] = Math.round(filter[i].commits/(filter[i].to-filter[i].from))
+    }
+    buildChart(chartName, chartTitle, labelTitle, labels, data, false);
+
+    // dataTableYear
+
+    /*
+    chartName = 'yearChart'
+    chartTitle =  'total commits per year'
+    labelTitle = 'commits'
+    data, labels = []
+    json = tableJson('dataTableYear')
+    labels = json.map(e => Object.values(e)[0])
+    data = json.map(e => e.total_commits)
+    BuildChart(chartName, chartTitle, labelTitle, data, true);
+    */
 }
