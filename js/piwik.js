3173aa649a09de42a2beef1a563c6af2b401acd1
silver <silv3rr@users.noreply.github.com>
2023-08-31 21:45:32 +0200

    syntax

--- js/piwik.js
+++ js/piwik.js
@@ -1,11 +1,11 @@
-var _paq = _paq || [];
+var _paq = window._paq = window._paq || [];
 /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
 (function() {
-  var u="//stats.pzs-ng.eu/piwik/";
-  _paq.push(['setTrackerUrl', u+'piwik.php']);
+  var u="https://stats.revlis.nl/piwik/";
+  _paq.push(['setTrackerUrl', u+'matomo.php']);
   _paq.push(['setSiteId', '18']);
   var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-  g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
 })();
