05434a3cecbeea35da77f68128a197e85c32655e
silver <silv3rr@users.noreply.github.com>
2024-11-02 20:29:19 +0100

    themes.js: add creator to themes list and downloads, merge custom in conf

--- js/themes.js
+++ js/themes.js
@@ -2,15 +2,12 @@ function createLinks(themesObj) {
     let addButtons = (url, names, creator, custom, sample) => {
         Object.values(names).forEach(name => {
             document.write(`<input class=\"lnkButton\" type=\"submit\" value=\"${name}\"
-                            onclick=\"return showTheme('${url}', '${name}', '${creator}', ${custom}, ${sample});\"/>`);
+                            onclick=\"return showTheme('${url}', '${name}', '${creator}', ${sample});\"/>`);
         });
     }
-    document.write(`<p>&nbsp;&nbsp o standard `);
-    addButtons(themesObj.default.url, themesObj.default.names, null, false, false);
-    document.write('</p>');
-    Object.keys(themesObj.custom).forEach(key => {
+    Object.keys(themesObj).forEach(key => {
         document.write(`<p>&nbsp;&nbsp; o ${key} `);
-        addButtons(themesObj.custom[key].url, themesObj.custom[key].names, key, true, themesObj.custom[key].sample);
+        addButtons(themesObj[key].url, themesObj[key].names, key, true, themesObj[key].sample);
         document.write('</p>');
     })
 }
@@ -20,10 +17,10 @@ function toggleMode() {
     document.getElementById('previewTxt').classList.toggle('toggleMode')
 }
 
-function showTheme(url, basename, creator, custom, sample) {
+function showTheme(url, basename, creator, sample) {
     document.getElementById('toggle').style.display = 'block';
     // try getting sample theme.log for theme.zst
-    if (custom && creator && sample) {
+    if (sample) {
         try {
             ircToHtml(`${url}${basename}.log`, function(text) {
                 document.getElementById('sample').style.display = 'block';
@@ -36,8 +33,8 @@ function showTheme(url, basename, creator, custom, sample) {
     ircToHtml(`${url}${basename}.zst`, function(text) {
         document.getElementById('previewTxt').innerHTML = `<pre>${text}</pre>`;
         document.getElementById('title').style.display = 'block';
-        document.getElementById('title').innerHTML = `<p>Download: <a href="${url}${basename}.zst">${basename}.zst</a>` +
-        `${(creator !== "null") ? ' by <strong>' + creator + '</strong>' : ""} ` +
-        `(${custom ? 'custom' : "bundled"})`;
+        document.getElementById('title').innerHTML =
+            `<p>Download: <a href="${url}${basename}.zst">${basename}.zst</a>` +
+            `${(creator !== "pzs-ng") ? ' by <strong>' + creator + '</strong>' : " (bundled)"}`
     });
 }
