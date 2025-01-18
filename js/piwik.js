bc01278fdb37199070c4836c3cd3f3204d1b43f2
silver <silv3rr@users.noreply.github.com>
2025-01-18 18:15:50 +0100

    fixup! update matomo url

--- js/piwik.js
+++ js/piwik.js
@@ -3,7 +3,7 @@ var _paq = window._paq = window._paq || [];
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
 (function() {
-  var u="https://stats.pzs-ng.eu/piwik/";
+  var u="https://stats.pzs-ng.eu/";
   _paq.push(['setTrackerUrl', u+'matomo.php']);
   _paq.push(['setSiteId', '18']);
   var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
